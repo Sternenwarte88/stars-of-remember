ee616147dbc31dddaad42ae29d6a78a5ea107ded
sternenwarte88 <s.loecher.slc@gmail.com>
2021-09-08 19:58:15 +0200

    Get styles ready

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,23 +1,29 @@
 // Form to send a star to the database
 import React from "react";
+import classes from "./form.module.css";
+import Button from "../button/buttons";
 
 export default function Forms() {
-  const buttonHandler = () => {
+  const buttonHandler = (event) => {
+    event.preventDefault();
     console.log("sending star to database");
   };
 
   return (
     <>
-      <form>
-        <label for="name">What's his/her name?</label>
+      <form className={classes.form}>
+        <label htmlFor="name">What's his/her name?</label>
         <input id="name" type="text" />
-        <label for="dob">When is he/she born?</label>
+        <label htmlFor="dob">When is he/she born?</label>
         <input id="dob" type="date" />
-        <label for="dod">When is he/she gone?</label>
+        <label htmlFor="dod">When is he/she gone?</label>
         <input id="dod" type="date" />
-        <button type="button" clicked={buttonHandler}>
-          Go little Star
-        </button>
+        <Button
+          clicked={(event) => {
+            buttonHandler(event);
+          }}
+          name="Go little Star"
+        />
       </form>
     </>
   );
