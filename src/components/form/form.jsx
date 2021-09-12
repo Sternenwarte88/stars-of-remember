ef950e030a8b1c5538489f0dc89b2f40c5229414
Sternenwarte88 <s.loecher.slc@gmail.com>
2021-09-12 20:34:38 +0200

    added refs for Inputfields

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,9 +1,13 @@
 // Form to send a star to the database
-import React from "react";
+import React, { useRef } from "react";
 import classes from "./form.module.css";
 import Button from "../button/buttons";
 
 export default function Forms() {
+  const nameRef = useRef();
+  const bodRef = useRef();
+  const dodRef = useRef();
+
   const buttonHandler = (event) => {
     event.preventDefault();
     console.log("sending star to database");
@@ -13,11 +17,11 @@ export default function Forms() {
     <>
       <form className={classes.form}>
         <label htmlFor="name">What's his/her name?</label>
-        <input id="name" type="text" />
+        <input id="name" type="text" ref={nameRef} />
         <label htmlFor="dob">When is he/she born?</label>
-        <input id="dob" type="date" />
+        <input id="dob" type="date" ref={bodRef} />
         <label htmlFor="dod">When is he/she gone?</label>
-        <input id="dod" type="date" />
+        <input id="dod" type="date" ref={dodRef} />
         <Button
           clicked={(event) => {
             buttonHandler(event);
